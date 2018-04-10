a719de86bf3bcd47a062ff793b23187a9b44a15e
RMADevelopment <info@sarahcheaib.de>
2018-04-10 14:56:02 +0200

    changed MongoDB URI

--- server.js
+++ server.js
@@ -17,9 +17,8 @@ var port = process.env.PORT || 8080;
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
 var uristring =
-    process.env.PORT ?
-        "mongodb://todo-rma-user:MangoMango1@ds139459.mlab.com:39459/todo-db" :
-        'mongodb://127.0.0.1/todo_app';
+    process.env.MONGOLAB_URI ||
+    'mongodb://127.0.0.1/todo_app';
 
 
 console.log('Trying to make express app listen to port: ' + port + " ...");
@@ -75,6 +74,7 @@ app.get('/api/tasks/:id', function (request, response) {
         }
     });
 });
+
 
 
 //add task items to database
